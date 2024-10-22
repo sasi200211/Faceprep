098e1307f3ef6d54937ac8f09679b2c4969cf444
SASI S P <81792622+sasi200211@users.noreply.github.com>
2024-10-22 11:22:58 +0530

    Add files via upload

---
+++ 22-10-2024/src/Info.js
@@ -0,0 +1,10 @@
+const Info = () => {
+    return (  
+        <div class="Info">
+            <h1>Info</h1>
+            <p> Sasi's blog here you will have the new series of diwali shopping</p>
+        </div>
+    );
+}
+ 
+export default Info;
